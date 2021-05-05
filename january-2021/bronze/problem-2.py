2b1d816a9135a1562faeec01cf46ab675a7ad976
Yash Singh <saiansh2525@gmail.com>
2021-05-04 20:56:15 -0700

    January 2021 Bronze Problem 2

---
+++ january-2021/bronze/problem-2.py
@@ -0,0 +1,25 @@
+n = raw_input()
+evenAndOddBreedIds = list(map(lambda num: num % 2 == 0, map(int, raw_input().split())))
+totalGroups = 0
+currentGroupType = True
+
+while True:
+  if currentGroupType:
+    if True in evenAndOddBreedIds:
+      evenAndOddBreedIds.pop(evenAndOddBreedIds.index(True))
+    elif len(list(filter(lambda cow: not(cow), evenAndOddBreedIds))) >= 2:
+      evenAndOddBreedIds.pop(evenAndOddBreedIds.index(False))
+      evenAndOddBreedIds.pop(evenAndOddBreedIds.index(False))
+    else:
+      totalGroups -= 1
+      break
+    totalGroups += 1
+  else:
+    if False in evenAndOddBreedIds:
+      evenAndOddBreedIds.pop(evenAndOddBreedIds.index(False))
+      totalGroups += 1
+    else:
+      break
+  currentGroupType = not(currentGroupType)
+
+print(totalGroups)
