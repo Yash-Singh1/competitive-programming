38082fc1ed1f901ce985f23326f682de070ded9b
Yash Singh <saiansh2525@gmail.com>
2021-05-05 14:58:14 -0700

    January 2021 Bronze Problem 3

---
+++ january-2021/bronze/problem-3.py
@@ -0,0 +1,13 @@
+n = raw_input()
+heights = map(int, raw_input().split())
+maxHeights = map(int, raw_input().split())
+
+def permutations(lst):
+  if len(lst) == 1:
+    return [lst]
+  permutationsLst = []
+  for index, element in enumerate(lst):
+    permutationsLst.extend(map(lambda permutation: [element] + permutation, permutations(lst[0 : index] + lst[index + 1 : len(lst)])))
+  return permutationsLst
+
+print(len(filter(lambda permutation: len(filter(lambda entryForPermutation: entryForPermutation[1] > maxHeights[entryForPermutation[0]], enumerate(permutation))) == 0, permutations(heights))))
