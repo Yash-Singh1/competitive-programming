f0f05b21a8c82cb935e3878aacf2e1a195c75dec
Yash-Singh1 <saiansh2525@gmail.com>
2022-02-11 15:49:23 -0800

    feat: +3 cpp hackerrank

---
+++ hackerrank/classes-objects/main.cpp
@@ -0,0 +1,46 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+class Student {
+private:
+int scores[5];
+public:
+void input() {
+    cin >> scores[0] >> scores[1] >> scores[2] >> scores[3] >> scores[4];
+}
+int calculateTotalScore() {
+    return scores[0] + scores[1] + scores[2] + scores[3] + scores[4];
+}
+};
+
+int main() {
+    int n; // number of students
+    cin >> n;
+    Student *s = new Student[n]; // an array of n students
+    
+    for(int i = 0; i < n; i++){
+        s[i].input();
+    }
+
+    // calculate kristen's score
+    int kristen_score = s[0].calculateTotalScore();
+
+    // determine how many students scored higher than kristen
+    int count = 0; 
+    for(int i = 1; i < n; i++){
+        int total = s[i].calculateTotalScore();
+        if(total > kristen_score){
+            count++;
+        }
+    }
+
+    // print result
+    cout << count;
+    
+    return 0;
+}
