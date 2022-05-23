ad479f279175a8fe64e839ab6ceb91c5f8bac794
Yash Singh <saiansh2525@gmail.com>
2022-05-23 07:10:12 -0700

    chore: explaining my code for future

--- cses/1661/main.cpp
+++ cses/1661/main.cpp
@@ -21,8 +21,13 @@ int main()
   m[0] = 1;
   for (auto e: a) {
       sum += e;
+      // NOTE: Uses Running Sum. For simplicity imagine that you are in a prefix sum with e being the original ans sum being the sum till now.
       ans += m[sum - x];
       m[sum]++;
   }
+  // The map contains the number of times a certain sum is filled up till.
+  // So if I have 7 0 3, with x = 10, the number of times it gets filled up is 2.
+  // Whenever ans is incremented, it gets incremented by the number of times the rest of the sum needed to fill is filled up already.
+  // Another example is 8 3 with x = 3. The prefix sum is 8 11. Since the sum of the subarray [1, 1] is 3. As expected 11 - 8 will give us the expected 3.
   std::cout << ans;
 }
