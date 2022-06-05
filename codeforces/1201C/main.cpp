67fb02d817a03b0dbbc4cbc760efa31b748440a3
Yash Singh <saiansh2525@gmail.com>
2022-06-04 20:58:43 -0700

    feat: 1201C optimizations

--- codeforces/1201C/main.cpp
+++ codeforces/1201C/main.cpp
@@ -1,5 +1,6 @@
 // WIP: Some sort of edge case Runtime Error, but main idea is down
 // Could have some simplicity by extracting ternaries into variables
+// UPDATE: Now has TLE and base test cases pass, still WIP on time complexity
 
 #include <bits/stdc++.h>
 
@@ -17,30 +18,44 @@ int main() {
         a.push_back(j);
     }
     sort(a.begin(), a.end());
+    // for (int i{0}; i < n; ++i) {
+    //     cout << a[i] << " ";
+    // }
+    // cout << endl;
     if (n == 1) {
         cout << (a[0] + k);
         return 0;
     }
-    bool odd{(n & 1) == 1};
+    // bool odd{(n & 1) == 1};
     ++k;
+    long long mid{(n / 2)};
     while (--k) {
         a[n / 2]++;
-        if (n > 1 && a[odd ? (n / 2) : (n / 2 - 1)] > a[(odd ? (n / 2) : (n / 2 - 1)) + 1]) {
-            long long c{a[n / 2]};
-            auto ub{upper_bound(a.begin(), a.end(), a[(odd ? (n / 2) : (n / 2 - 1)) + 1])};
+        // for (int i{0}; i < n; ++i) {
+        //     cout << a[i] << " ";
+        // }
+        // cout << endl;
+        if (a[mid] > a[(mid) + 1]) {
+            long long c{a[n / 2]}; // switch storage
+            auto ub{upper_bound(a.begin() + mid, a.end(), a[(mid) + 1])};
+            //cout << a[(mid) + 1] << " " << ub - a.begin() << endl;
             if (ub == a.end()) {
-                a[odd ? (n / 2) : (n / 2 - 1)] = a[n - 1];
+                a[mid] = a[n - 1];
                 a[n - 1] = c;
             } else {
-                a[odd ? (n / 2) : (n / 2 - 1)] = a[ub - a.begin() - 1];
+                a[mid] = a[ub - a.begin() - 1];
                 a[ub - a.begin() - 1] = c;
             }
+            // for (int i{0}; i < n; ++i) {
+            //     cout << a[i] << " ";
+            // }
+            // cout << endl;
         }
     }
-    if (odd) {
+    // if (odd) {
         cout << a[n / 2];
-    } else {
-        cout << (a[(n / 2) - 1] + a[(n / 2)]) / 2;
-    }
+    // } else {
+        // cout << (a[(n / 2) - 1] + a[(n / 2)]) / 2;
+    // }
     return 0;
 }
