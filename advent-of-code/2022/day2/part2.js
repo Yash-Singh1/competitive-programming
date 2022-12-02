5a5fcdd772c6325868a3166214ad5cb574889026
Yash-Singh1 <saiansh2525@gmail.com>
2022-12-01 21:08:45 -0800

    feat: advent of code speedrun day 2

---
+++ advent-of-code/2022/day2/part2.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+
+const inned = fs.readFileSync('input', 'utf-8').trim();
+
+const all = inned.split('\n');
+
+let vgal = 0
+all.forEach(sm => {
+  sm = sm.split(' ');
+  if (sm[0] == 'A') sm[0] = 1;
+  if (sm[0] == 'B') sm[0] = 2;
+  if (sm[0] == 'C') sm[0] = 3;
+  if (sm[1] == 'X') sm[1] = 1;
+  if (sm[1] == 'Y') sm[1] = 2;
+  if (sm[1] == 'Z') sm[1] = 3;
+  if (sm[1] == 1) {
+    if (sm[0] == 1) vgal += 3
+    else if (sm[0] == 2) vgal += 1
+    else if (sm[0] == 3) vgal += 2
+  } else if (sm[1] == 2) {
+    vgal += 3 + sm[0];
+  } else {
+    if (sm[0] == 1) vgal += 2
+    else if (sm[0] == 2) vgal += 3
+    else if (sm[0] == 3) vgal += 1
+    vgal += 6;
+  }
+})
+
+console.log(vgal)
