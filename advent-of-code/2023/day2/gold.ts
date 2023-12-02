9cb8e69ef655c377739998bb4e63435eaf89de43
Yash Singh <saiansh2525@gmail.com>
2023-12-01 22:18:05 -0800

    feat: advent of code day 2

---
+++ advent-of-code/2023/day2/gold.ts
@@ -0,0 +1,47 @@
+import fs from "node:fs";
+
+const DELIM = "\n";
+
+const input = fs.readFileSync("message (4).txt", "utf-8");
+const lines = input.split(DELIM);
+const trimmed = input.trim();
+
+let sum = 0;
+
+const red = 12;
+const green = 13;
+const blue = 14;
+
+const mx: Record<string,number> = {
+  red,
+  green,
+  blue
+}
+
+trimmed.split(DELIM).forEach((line, idx) => {
+  let linel = line
+    .split(":")[1]
+    .trim()
+    .split(";")
+    .map((str) => str.trim())
+    .map((str) => {
+      return str.split(",").map((val) => {
+        return val.trim().split(" ").map(val2 => val2.trim());
+      });
+    });
+
+  let mxv: Record<string,number> = {
+    red: 0,
+    green: 0,
+    blue: 0
+  };
+  $: for (let i = 0; i < linel.length; ++i) {
+    for (let j = 0; j < linel[i].length; ++j) {
+      mxv[linel[i][j][1]] = Math.max(mxv[linel[i][j][1]], parseInt(linel[i][j][0], 10))
+    }
+  }
+
+  sum += mxv.red * mxv.green * mxv.blue;
+});
+
+console.log(sum)
---
+++ advent-of-code/2023/day2/silver.ts
@@ -0,0 +1,46 @@
+import fs from "node:fs";
+
+const DELIM = "\n";
+
+const input = fs.readFileSync("message (4).txt", "utf-8");
+const lines = input.split(DELIM);
+const trimmed = input.trim();
+
+let sum = 0;
+
+const red = 12;
+const green = 13;
+const blue = 14;
+
+const mx: Record<string,number> = {
+  red,
+  green,
+  blue
+}
+
+trimmed.split(DELIM).forEach((line, idx) => {
+  let linel = line
+    .split(":")[1]
+    .trim()
+    .split(";")
+    .map((str) => str.trim())
+    .map((str) => {
+      return str.split(",").map((val) => {
+        return val.trim().split(" ").map(val2 => val2.trim());
+      });
+    });
+
+  let valid = true;
+  $: for (let i = 0; i < linel.length; ++i) {
+    for (let j = 0; j < linel[i].length; ++j) {
+      if (parseInt(linel[i][j][0], 10) > mx[linel[i][j][1]]) {
+        valid = false;
+        break $;
+      }
+    }
+  }
+
+  if (valid) sum += idx+1;
+});
+
+console.log(sum)
