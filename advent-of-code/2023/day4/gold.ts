9fc8d0146556512c56cba07dba9a654b8417bfe1
Yash Singh <saiansh2525@gmail.com>
2023-12-03 22:28:09 -0800

    feat: day 4 aoc

---
+++ advent-of-code/2023/day4/gold.ts
@@ -0,0 +1,48 @@
+import assert from "node:assert";
+import fs from "node:fs";
+
+const DELIM = "\n";
+
+const input = fs.readFileSync("in.txt", "utf-8");
+const trimmed = input.trim();
+const lines = trimmed.split(DELIM);
+
+let sum = 0;
+
+const memo = new Map<string, number>();
+
+function matches(line: string, i: number) {
+  if (memo.has(line.split(': ')[0])) {
+    return memo.get(line.split(': ')[0])!;
+  }
+  let cards = line.split(': ')[0];
+  line = line.split(': ')[1]
+  const [win, have] = line.split('|').map(x => x.trim().split(' ').filter(Boolean).map(y => {
+    return Number(y.trim())
+  }));
+  const s = new Set<number>();
+  for (const num of win) {
+    assert(!isNaN(num), );
+    s.add(num);
+  }
+  let val = 0;
+  for (const num of have) {
+    assert(!isNaN(num), );
+    if (s.has(num)) {
+      ++val;
+    }
+  }
+  let rr = 1;
+  for (let j = i+1; j <= i+val && j < lines.length; ++j) {
+    rr += matches(lines[j], j);
+  }
+  memo.set(cards, rr);
+  return rr;
+}
+
+trimmed.split(DELIM).map((x, i) => ({ x, i })).forEach(({x: line, i}) => {
+  console.log(i, matches(line, i));
+  sum += matches(line, i);
+});
+
+console.log(sum)
---
+++ advent-of-code/2023/day4/silver.ts
@@ -0,0 +1,34 @@
+import assert from "node:assert";
+import fs from "node:fs";
+
+const DELIM = "\n";
+
+const input = fs.readFileSync("in.txt", "utf-8");
+const lines = input.split(DELIM);
+const trimmed = input.trim();
+
+let sum = 0;
+
+trimmed.split(DELIM).forEach((line, idx) => {
+  line = line.split(': ')[1]
+  const [win, have] = line.split('|').map(x => x.trim().split(' ').filter(Boolean).map(y => {
+    return Number(y.trim())
+  }));
+  const s = new Set<number>();
+  for (const num of win) {
+    assert(!isNaN(num), );
+    s.add(num);
+  }
+  let val = 0;
+  for (const num of have) {
+    assert(!isNaN(num), );
+    if (s.has(num)) {
+      if (val === 0) val = 1;
+      else val *= 2;
+    }
+  }
+  console.log(val);
+  sum += val;
+});
+
+console.log(sum)
