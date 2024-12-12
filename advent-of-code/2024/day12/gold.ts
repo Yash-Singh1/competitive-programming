d938679ab1423d37044e45d5d13eb570b2618746
Yash Singh <saiansh2525@gmail.com>
2024-12-12 07:17:33 -0800

    chore: cleanup gold

--- advent-of-code/2024/day12/gold.ts
+++ advent-of-code/2024/day12/gold.ts
@@ -9,8 +9,6 @@ let grid = text.split("\n").map((line) => line.split(""));
 let regions = [...grid.map((line) => line.map((char) => -1))];  
 let regionCount = 0;
 
-console.log(grid, regions)
-
 for (let i = 0; i < grid.length; i++) {
   for (let j = 0; j < grid[i].length; j++) {
     if (regions[i][j] === -1) {
@@ -38,34 +36,6 @@ for (let i = 0; i < grid.length; i++) {
       const exposedSet = new Set<string>();
       for (const regionCoord of regionSet) {
         const [x, y] = regionCoord.split(",").map((x) => parseInt(x)) as [number, number]; 
-        // let exposedVertical = false;
-        // for (let [dx, dy] of [[1, 0], [-1, 0]]) {
-        //   if (!(x + dx >= 0 && x + dx < grid.length && y + dy >= 0 && y + dy < grid[0].length && grid[x + dx][y + dy] === grid[i][j])) {
-        //     exposedVertical = true;
-        //   }
-        // }
-        // let exposedHorizontal = false;
-        // for (let [dx, dy] of [[0, 1], [0, -1]]) {
-        //   if (!(x + dx >= 0 && x + dx < grid.length && y + dy >= 0 && y + dy < grid[0].length && grid[x + dx][y + dy] === grid[i][j])) {
-        //     exposedHorizontal = true;
-        //   }
-        // }
-        // if (exposedHorizontal) {
-        //   let x2 = x - 1;
-        //   while (x2 >= 0 && x2 < grid.length && grid[x2][y] === grid[i][j]) {
-        //     x2--;
-        //   }
-        //   if (x2 < 0) x2 = 0;
-        //   exposedHorizontalSet.add([x2, y].join(","));
-        // }
-        // if (exposedVertical) {
-        //   let y2 = y - 1;
-        //   while (y2 >= 0 && y2 < grid[0].length && grid[x][y2] === grid[i][j]) {
-        //     y2--;
-        //   }
-        //   if (y2 < 0) y2 = 0;
-        //   exposedHorizontalSet.add([x, y2].join(","));
-        // }
         for (let [dx, dy] of [[1, 0], [-1, 0], [0, 1], [0, -1]]) {
           if (!(x + dx >= 0 && x + dx < grid.length && y + dy >= 0 && y + dy < grid[0].length && grid[x + dx][y + dy] === grid[i][j])) {
             if (dx === 0) {
@@ -88,18 +58,9 @@ for (let i = 0; i < grid.length; i++) {
         }
       }
       regionCount++;
-      console.log(i, j, area, perimeter, exposedSet);
       target += exposedSet.size * area;
     }
   }
-}
-
-for (let i = 0; i < grid.length; i++) {
-  let line = ""
-  grid[i].forEach((_, j) => {
-    line += regions[i][j].toString().padStart(2, "0") + ",";
-  });
-  console.log(line);
 }
 
 console.log(target);
